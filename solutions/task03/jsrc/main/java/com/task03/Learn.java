a1bfe563f2989bd8c512757eae158b3e7e3a9538
Jesus_Morales <andresmor93@msn.com>
2024-11-14 01:00:55 -0600

    Fixed put key

--- solutions/task03/jsrc/main/java/com/task03/Learn.java
+++ solutions/task03/jsrc/main/java/com/task03/Learn.java
@@ -20,8 +20,8 @@ public class Learn implements RequestHandler<Object, Map<String, Object>> {
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("message", "Hello from Lambda");
 		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
 		return resultMap;
 	}
 }
