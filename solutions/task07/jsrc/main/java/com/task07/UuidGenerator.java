05a968ac286887a80d865f9525ed047c088b4adc
Jesus_Morales <andresmor93@msn.com>
2024-11-16 11:01:55 -0600

    First commit

---
+++ solutions/task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -0,0 +1,28 @@
+package com.task07;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(
+    lambdaName = "uuid_generator",
+	roleName = "uuid_generator-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@EnvironmentVariable(key = "target_bucket", value = "${target_bucket}")
+public class UuidGenerator implements RequestHandler<Void, Void> {
+	private final String targetBucket = System.getenv("target_bucket");
+
+	public Void handleRequest(Void request, Context context) {
+
+		return null;
+	}
+}
