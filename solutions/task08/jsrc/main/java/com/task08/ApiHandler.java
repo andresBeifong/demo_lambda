26cf88329bf527929da0cb80a7738a325207d3db
Jesus_Morales <andresmor93@msn.com>
2024-11-18 14:54:19 -0600

    Added Lambda URL

--- solutions/task08/jsrc/main/java/com/task08/ApiHandler.java
+++ solutions/task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -2,14 +2,19 @@ package com.task08;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task08.lambdalayer.OpenMeteoAPI;
 
-import java.io.IOException;
+import java.util.Map;
 
 @LambdaHandler(
         lambdaName = "api_handler",
@@ -24,15 +29,28 @@ import java.io.IOException;
         runtime = DeploymentRuntime.JAVA11,
         artifactExtension = ArtifactExtension.ZIP,
         libraries = "lib/open-meteo-service-1.0-SNAPSHOT.jar"
+)@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
-public class ApiHandler implements RequestHandler<Object, String> {
+public class ApiHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     private final OpenMeteoAPI weatherAPI = new OpenMeteoAPI();
+    private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
 
-    public String handleRequest(Object request, Context context) {
-        try {
-            return weatherAPI.getData();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        APIGatewayV2HTTPEvent.RequestContext.Http http = event.getRequestContext().getHttp();
+        if("GET".equals(http.getMethod()) && http.getPath().contains("/weather")){
+            String weatherData = weatherAPI.getData();
+            return APIGatewayV2HTTPResponse.builder()
+                    .withBody(weatherData)
+                    .withStatusCode(200)
+                    .withHeaders(responseHeaders)
+                    .build();
         }
+        return APIGatewayV2HTTPResponse.builder()
+                .withBody("Path not found")
+                .withStatusCode(404)
+                .build();
     }
 }
