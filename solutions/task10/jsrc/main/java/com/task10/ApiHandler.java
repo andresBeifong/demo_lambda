b83d5d94c84a9cca1ccc0dbcc6ba7e6ede94692e
Jesus_Morales <andresmor93@msn.com>
2024-11-18 21:35:47 -0600

    Initial configuration and classes to handle http request

--- solutions/task10/jsrc/main/java/com/task10/ApiHandler.java
+++ solutions/task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -2,14 +2,20 @@ package com.task10;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.task10.dto.RouteKey;
+import com.task10.handler.*;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static com.syndicate.deployment.model.environment.ValueTransformer.USER_POOL_NAME_TO_CLIENT_ID;
@@ -28,13 +34,61 @@ import static com.syndicate.deployment.model.environment.ValueTransformer.USER_P
 		@EnvironmentVariable(key = "COGNITO_ID", value = "${pool_name}", valueTransformer = USER_POOL_NAME_TO_USER_POOL_ID),
 		@EnvironmentVariable(key = "CLIENT_ID", value = "${pool_name}", valueTransformer = USER_POOL_NAME_TO_CLIENT_ID)
 })
-public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
+public class ApiHandler implements  RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	private final CognitoIdentityProviderClient cognitoClient;
+	private final Map<RouteKey, RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>> handlersByRouteKey;
+	private final Map<String, String> headersForCORS;
+	private final RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> routeNotImplementedHandler;
+
+	public ApiHandler() {
+		this.cognitoClient = initCognitoClient();
+		this.handlersByRouteKey = initHandlers();
+		this.headersForCORS = initHeadersForCORS();
+		this.routeNotImplementedHandler = new RouteNotImplementedHandler();
+	}
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+		return getHandler(requestEvent)
+				.handleRequest(requestEvent, context)
+				.withHeaders(headersForCORS);
+	}
+
+	private RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> getHandler(APIGatewayProxyRequestEvent requestEvent) {
+		return handlersByRouteKey.getOrDefault(getRouteKey(requestEvent), routeNotImplementedHandler);
+	}
+
+	private RouteKey getRouteKey(APIGatewayProxyRequestEvent requestEvent) {
+		return new RouteKey(requestEvent.getHttpMethod(), requestEvent.getPath());
+	}
+
+	private CognitoIdentityProviderClient initCognitoClient() {
+		return CognitoIdentityProviderClient.builder()
+				.region(Region.of(System.getenv("REGION")))
+				.credentialsProvider(DefaultCredentialsProvider.create())
+				.build();
+	}
+
+	private Map<RouteKey, RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>> initHandlers() {
+		return Map.of(
+				new RouteKey("GET", "/"), new GetRootHandler(),
+				new RouteKey("POST", "/signup"), new PostSignUpHandler(cognitoClient),
+				new RouteKey("POST", "/signin"), new PostSignInHandler(cognitoClient),
+				new RouteKey("GET", "/tables"), new GetTablesHandler()
+		);
+	}
+
+	/**
+	 * To allow all origins, all methods, and common headers
+	 * <a href="https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html">Using cross-origin resource sharing (CORS)</a>
+	 */
+	private Map<String, String> initHeadersForCORS() {
+		return Map.of(
+				"Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+				"Access-Control-Allow-Origin", "*",
+				"Access-Control-Allow-Methods", "*",
+				"Accept-Version", "*"
+		);
 	}
 }
